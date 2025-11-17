d8f222de82590654d3ad2f6936844a0c4555c631
Michał Bąk <michalbak000@gmail.com>
2025-11-17 18:51:37 +0100

    [SCRAPER] Refactoring

--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 import os
 import requests
+
 from bs4 import BeautifulSoup
 from bs4.element import Tag, ResultSet
 from dotenv import load_dotenv
 from typing import List, Dict, Optional, Any
 
-load_dotenv()
+
 
 class Scraper:
 
@@ -27,6 +28,8 @@ class Scraper:
     # Pobiera html i scrapuje bloki lekcyjne
     def __init__(self) -> None:
 
+        load_dotenv()
+
         self.URL = os.environ.get("URL")
         if not self.URL:
             raise EnvironmentError("URL environment variable not set")
@@ -47,7 +50,7 @@ class Scraper:
         self.blocks = []
 
     # Filtruje i formatuje bloki zajęciowe
-    def getBlocks(self) -> List[Dict[str, str]]:
+    def get_blocks(self) -> List[Dict[str, str]]:
 
         # Filtruj lekcje na podstawie daty
         for lesson in self.lessons:
@@ -76,7 +79,7 @@ class Scraper:
 
             # 2. Wpisz dane do bloku
             block["date"] = lesson.find("span", class_="date").text
-            block["block_id"] = lesson.find("span", class_="block_id").text[5]
+            block["block_id"] = lesson.find("span", class_="block_id").text
             block["name"] = lesson.find("span", class_="name").text
             block["info"] = lesson.find("span", class_="info").text
             block["color"] = lesson.find("span", class_="colorp").text
