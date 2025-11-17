c29b018e5e74e91ee5b199d682fa1ce0e6088258
Michał Bąk <michalbak000@gmail.com>
2025-11-17 17:00:51 +0100

    [SCRAPER] Added scraper obj definition

--- scraper.py
+++ scraper.py
@@ -1,53 +1,87 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from bs4.element import Tag, ResultSet
 from dotenv import load_dotenv
+from typing import List, Dict, Optional, Any
 
 load_dotenv()
 
-URL = os.environ.get("URL")
-START_DATE = os.environ.get("START_DATE")
-END_DATE = os.environ.get("END_DATE")
-page = requests.get(URL)
-soup = BeautifulSoup(page.text, "html.parser")
-lessons = soup.find_all("div", class_="lesson")
-lessons_in_sem = []
-blocks = []
+class scraper:
 
+    # 'Optional[str]' oznacza, że typem jest 'str' lub 'None'
+    # (bo os.environ.get() zwraca None, jeśli nie znajdzie zmiennej)
+    URL: Optional[str]
+    START_DATE: Optional[str]
+    END_DATE: Optional[str]
 
+    # ResultSet[Any] to typ, który zwraca soup.find_all(...)
+    lessons: ResultSet[Any]
 
-# Filtruj lekcje na podstawie daty
-for lesson in lessons:
+    # List[Tag] to lista elementów HTML, które pasują do naszych dat
+    lessons_in_sem: List[Tag]
 
-    # 1. Znajdź span z datą wewnątrz elementu 'lesson'
-    date_span = lesson.find("span", class_="date")
+    # 'Dict[str, str]' jest bardziej precyzyjne niż 'dict' (mówimy, że klucze i wartości są stringami)
+    blocks: List[Dict[str, str]]
 
-    # 2. Pomiń elementy, które z jakiegoś powodu nie mają daty
-    if not date_span:
-        continue
+    # Pobiera html i scrapuje bloki lekcyjne
+    def __init__(self) -> None:
 
-    # 3. Wyciągnij tekst i usuń białe znaki
-    #    "  2026_03_05  " -> "2026_03_05"
-    date_str = date_span.get_text(strip=True)
+        self.URL = os.environ.get("URL")
+        if not self.URL:
+            raise EnvironmentError("URL environment variable not set")
 
-    # 4. Porównaj daty (jako stringi)
-    if START_DATE <= date_str <= END_DATE:
-        # Jeśli data mieści się w przedziale, dodaj element do nowej listy
-        lessons_in_sem.append(lesson)
+        self.START_DATE = os.environ.get("START_DATE")
+        if not self.START_DATE:
+            raise EnvironmentError("START_DATE environment variable not set")
 
+        self.END_DATE = os.environ.get("END_DATE")
+        if not self.END_DATE:
+            raise EnvironmentError("END_DATE environment variable not set")
 
-# Formatuj informacje o bloku zajec
-for lesson in lessons_in_sem:
+        page: requests.Response = requests.get(self.URL)
+        soup: BeautifulSoup = BeautifulSoup(page.text, "html.parser")
 
-    # 1. Utwórz słownik na informacje o bloku zajeć
-    block = {}
+        self.lessons = soup.find_all("div", class_="lesson")
+        self.lessons_in_semester = []
+        self.blocks = []
 
-    # 2. Wpisz dane do bloku
-    block["date"] = lesson.find("span", class_="date").text
-    block["block_id"] = lesson.find("span", class_="block_id").text[5]
-    block["name"] = lesson.find("span", class_="name").text
-    block["info"] = lesson.find("span", class_="info").text
-    block["color"] = lesson.find("span", class_="colorp").text
+    # Filtruje i formatuje bloki zajęciowe
+    def get_blocks_in_range(self) -> List[Dict[str, str]]:
 
-    # 3. Dodaj blok do listy bloków
-    blocks.append(block)
+        # Filtruj lekcje na podstawie daty
+        for lesson in self.lessons:
+
+            # 1. Znajdź span z datą wewnątrz elementu 'lesson'
+            date_span = lesson.find("span", class_="date")
+
+            # 2. Pomiń elementy, które z jakiegoś powodu nie mają daty
+            if not date_span:
+                continue
+
+            # 3. Wyciągnij tekst i usuń białe znaki
+            #    "  2026_03_05  " -> "2026_03_05"
+            date_str = date_span.get_text(strip=True)
+
+            # 4. Porównaj daty (jako stringi)
+            if self.START_DATE <= date_str <= self.END_DATE:
+                # Jeśli data mieści się w przedziale, dodaj element do nowej listy
+                self.lessons_in_semester.append(lesson)
+
+        # Formatuj informacje o bloku zajec
+        for lesson in self.lessons_in_semester:
+
+            # 1. Utwórz słownik na informacje o bloku zajeć
+            block = {}
+
+            # 2. Wpisz dane do bloku
+            block["date"] = lesson.find("span", class_="date").text
+            block["block_id"] = lesson.find("span", class_="block_id").text[5]
+            block["name"] = lesson.find("span", class_="name").text
+            block["info"] = lesson.find("span", class_="info").text
+            block["color"] = lesson.find("span", class_="colorp").text
+
+            # 3. Dodaj blok do listy bloków
+            self.blocks.append(block)
+
+        return self.blocks
